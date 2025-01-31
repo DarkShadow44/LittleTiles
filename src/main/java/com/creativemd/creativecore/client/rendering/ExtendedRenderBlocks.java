c7503a40905bf420cb7f054e36ca68c1a9868afc
Fabian Maurer <dark.shadow4@web.de>
2025-01-31 19:07:10 +0100

    Move items into own creative tab

--- src/main/java/com/creativemd/creativecore/client/rendering/ExtendedRenderBlocks.java
+++ src/main/java/com/creativemd/creativecore/client/rendering/ExtendedRenderBlocks.java
@@ -1,7 +1,6 @@
 package com.creativemd.creativecore.client.rendering;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.util.IIcon;
